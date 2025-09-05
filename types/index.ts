074e7131d2e72f2d742525dd838f15491bc10d16
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-09-04 22:27:53 -0400

    fix: profle update type

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
-import { updateUserSchema } from "@/lib/validators";
+import { updateProfileSchema, updateUserSchema } from "@/lib/validators";
 
 export type UpdateUser = z.infer<typeof updateUserSchema>;
+export type UpdateProfile = z.infer<typeof updateProfileSchema>;
