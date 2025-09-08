c70c3d1202269ad6d81e5edc66b7e0157643cc49
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-09-08 08:19:40 -0400

    feat: header for about page

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,5 +1,20 @@
+import Logo from "@/components/shared/logo";
+import Image from "next/image";
+
 const AboutPage = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <article>
+        <header className="bg-blue-300 flex">
+          <Logo />
+          <div>
+            <h2>CT'S FIRST GOLF CLUB WITHOUT REAL ESTATE</h2>
+            <h4>EST. 1989</h4>
+          </div>
+        </header>
+      </article>
+    </>
+  );
 };
 
 export default AboutPage;
