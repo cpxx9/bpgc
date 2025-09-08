64b77fe407b6df2a420f9d9a1c99b07261bfd721
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-09-08 12:53:22 -0400

    feat: Logo compononent more reusable, can take in classes and height/width

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -2,23 +2,30 @@ import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
 import darkLogo from "@/assets/logo-dark.png";
 import lightLogo from "@/assets/logo.png";
+import { cn } from "@/lib/utils";
 
-const Logo = () => {
+interface PropTypes {
+  className?: string;
+  height?: number;
+  width?: number;
+}
+
+const Logo = ({ className = "", height = 48, width = 48 }: PropTypes) => {
   return (
     <>
       <Image
         src={darkLogo}
-        className="hidden dark:block"
-        width={48}
-        height={48}
+        className={cn(className, "hidden dark:block")}
+        width={width}
+        height={height}
         alt={`${APP_NAME} dark mode logo`}
         priority={true}
       />
       <Image
         src={lightLogo}
-        className="block dark:hidden"
-        width={48}
-        height={48}
+        className={cn(className, "block dark:hidden")}
+        width={width}
+        height={height}
         alt={`${APP_NAME} light modelogo`}
         priority={true}
       />
