a5a2a1a7fa4b24371c34c341056be460d819be4d
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-09-08 12:49:19 -0400

    feat: heading section about page

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -6,11 +6,11 @@ const AboutPage = () => {
     <>
       <section className="bg-blue-300 flex-col flex-center p-4 text-white text-3xl font-semibold">
         <Logo />
-        <h2 className="text-center">
+        <h2 className="text-center mt-2">
           CT'S FIRST GOLF CLUB WITHOUT REAL ESTATE
         </h2>
         <hr className="w-11 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700"></hr>
-        <h4>EST. 1989</h4>
+        <h4 className="pb-3">EST. 1989</h4>
       </section>
       <article></article>
     </>
