6774523f836ca7895607dd7ca3a1aa5199c45a4c
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-09-08 17:18:13 -0400

    feat: section for logo and heading complete

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 const AboutPage = () => {
   return (
     <>
-      <section className="bg-blue-300 flex-col flex-center p-4 text-white text-3xl font-semibold">
-        <Logo />
-        <h2 className="text-center mt-2">
+      <section className="bg-blue-300 flex-col flex-center p-4 text-white font-semibold">
+        <Logo width={116} height={116} />
+        <h2 className="text-center mt-3 text-[2.1em]/[2rem]">
           CT'S FIRST GOLF CLUB WITHOUT REAL ESTATE
         </h2>
         <hr className="w-11 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700"></hr>
-        <h4 className="pb-3">EST. 1989</h4>
+        <h4 className="pb-3 text-[1.8em]/[1.8rem]">EST. 1989</h4>
       </section>
       <article></article>
     </>
