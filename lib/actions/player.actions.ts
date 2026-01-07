d163a20ffd7830d489a5728e4ace0c4fe9cd387b
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-01-07 06:29:06 -0500

    feat: get player count action

--- lib/actions/player.actions.ts
+++ lib/actions/player.actions.ts
@@ -1,3 +1,6 @@
+"use server";
+
+import { prisma } from "@/db/prisma";
 import { requireAdminAction } from "@/lib/auth-guard";
 import { formatError } from "@/lib/utils";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
@@ -11,6 +14,18 @@ export async function createPlayer(formData: FormData) {
       throw error;
     }
 
+    return { success: false, message: formatError(error) };
+  }
+}
+
+export async function getPlayerCount() {
+  try {
+    const admin = await requireAdminAction();
+    if (!admin) throw new Error("You are not authorized!");
+    const playerCount = await prisma.player.count();
+
+    return { success: true, playerCount };
+  } catch (error) {
     return { success: false, message: formatError(error) };
   }
 }
