3f6f22bddcb8ad97df2bf1d2ced1a618b83d8615
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-01-07 07:10:39 -0500

    feat: getAll golfers function

--- lib/actions/golfer.actions.ts
+++ lib/actions/golfer.actions.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from "@/db/prisma";
 import { requireAdminAction } from "@/lib/auth-guard";
+import { PAGE_SIZE } from "@/lib/constants";
 import { formatError } from "@/lib/utils";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
@@ -27,5 +28,36 @@ export async function getGolferCount() {
     return { success: true, golferCount };
   } catch (error) {
     return { success: false, message: formatError(error) };
+  }
+}
+
+export async function getAllGolfers({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  try {
+    const admin = await requireAdminAction();
+    if (!admin) throw new Error("You are not authorized!");
+    const data = await prisma.golfer.findMany({
+      orderBy: { createdAt: "desc" },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    const dataCount = await prisma.user.count();
+
+    return {
+      success: true,
+      data,
+      totalPages: Math.ceil(dataCount / limit),
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
   }
 }
