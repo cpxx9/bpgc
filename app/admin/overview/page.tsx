ebc11ff2bb85e388fcb6ec2899cd4d5c320f00b5
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-01-07 06:43:08 -0500

    style: add shadow on hover over card links

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -5,6 +5,7 @@ import { requireAdmin } from "@/lib/auth-guard";
 import { formatNumber } from "@/lib/utils";
 import { Users, LandPlot } from "lucide-react";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -19,28 +20,32 @@ const AdminOverviewPage = async () => {
     <div className="space-y-2">
       <h1 className="h2-bold">Dashboard</h1>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Users</CardTitle>
-            <Users />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {formatNumber(userCount || 0)}
-            </div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Golfers</CardTitle>
-            <LandPlot />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              {formatNumber(golferCount || 0)}
-            </div>
-          </CardContent>
-        </Card>
+        <Link href={"/admin/users"}>
+          <Card className="hover:shadow-md">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Users</CardTitle>
+              <Users />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatNumber(userCount || 0)}
+              </div>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link href={"/admin/golfers"}>
+          <Card className="hover:shadow-md">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Golfers</CardTitle>
+              <LandPlot />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatNumber(golferCount || 0)}
+              </div>
+            </CardContent>
+          </Card>
+        </Link>
       </div>
     </div>
   );
