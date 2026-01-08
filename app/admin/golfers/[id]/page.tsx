10801049a620ae4e096dd5c336a841818ab4c12e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-01-07 21:04:44 -0500

    feat: update golfer page

---
+++ app/admin/golfers/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getGolferById } from "@/lib/actions/golfer.actions";
+import UpdateGolferForm from "@/components/admin/update-golfer-form";
+import { requireAdmin } from "@/lib/auth-guard";
+
+export const metadata: Metadata = {
+  title: "Update Golfer",
+};
+
+interface PropTypes {
+  params: Promise<{ id: string }>;
+}
+
+const AdminGolferUpdatePage = async ({ params }: PropTypes) => {
+  await requireAdmin();
+  const { id } = await params;
+
+  const golfer = await getGolferById(id);
+
+  if (!golfer) notFound();
+
+  return (
+    <div className="space-y-8 max-w-lg mx-auto">
+      <h1 className="h2-bold">
+        Update User <span className="text-sm">({id})</span>
+      </h1>
+      <UpdateGolferForm golfer={golfer} />
+    </div>
+  );
+};
+
+export default AdminGolferUpdatePage;
