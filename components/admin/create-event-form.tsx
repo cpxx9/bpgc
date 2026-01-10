a3e8258c60246816b50a5787b7dd455d7e48a882
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-01-10 07:45:17 -0500

    feat: start create event form

---
+++ components/admin/create-event-form.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { useSearchParams } from "next/navigation";
+import { createGolfer } from "@/lib/actions/golfer.actions";
+
+const CreateEventForm = () => {
+  const [data, action] = useActionState(createGolfer, {
+    success: false,
+    message: "",
+  });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  const CreateButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button
+        form="create-golfer-form"
+        type="submit"
+        disabled={pending}
+        className="w-full"
+        variant="default"
+      >
+        {pending ? "Creating..." : "Save"}
+      </Button>
+    );
+  };
+
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button>+ Create</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Create new golfer</DialogTitle>
+            <DialogDescription>
+              Enter golfer information here. Click save to create.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <form id="create-golfer-form" action={action}>
+              <input type="hidden" name="callbackUrl" value={callbackUrl} />
+              <div className="space-y-6">
+                <div>
+                  <Label htmlFor="firstName">First Name</Label>
+                  <Input
+                    id="firstName"
+                    name="firstName"
+                    type="text"
+                    required
+                    autoComplete="firstname"
+                  ></Input>
+                </div>
+                <div>
+                  <Label htmlFor="lastName">Last Name</Label>
+                  <Input
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    required
+                    autoComplete="lastName"
+                  ></Input>
+                </div>
+                <div>
+                  <Label htmlFor="hci">HCI</Label>
+                  <Input
+                    id="hci"
+                    name="hci"
+                    type="decimal"
+                    required
+                    autoComplete="hci"
+                  ></Input>
+                </div>
+                {/* <div>
+                  <Label htmlFor="twoManTeam">Two Man Team</Label>
+                  <Input
+                    id="twoManTeam"
+                    name="twoManTeam"
+                    type="text"
+                    autoComplete="twoManTeam"
+                  ></Input>
+                </div> */}
+
+                {data && !data.success && (
+                  <div className="text-center text-destructive">
+                    {data.message}
+                  </div>
+                )}
+              </div>
+            </form>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <CreateButton />
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+};
+
+export default CreateEventForm;
