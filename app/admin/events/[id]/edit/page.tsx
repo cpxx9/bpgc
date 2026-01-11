b2d778b3b5ca0d4aa1e6aa54a4b7ceb7bb9b7f41
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-01-11 13:10:19 -0500

    refactor: move update event page to /edit

---
+++ app/admin/events/[id]/edit/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import UpdateEventForm from "@/components/admin/update-event-form";
+import { requireAdmin } from "@/lib/auth-guard";
+import { getEventById } from "@/lib/actions/event.actions";
+
+export const metadata: Metadata = {
+  title: "Update Event",
+};
+
+interface PropTypes {
+  params: Promise<{ id: string }>;
+}
+
+const AdminEventUpdatePage = async ({ params }: PropTypes) => {
+  await requireAdmin();
+  const { id } = await params;
+
+  const event = await getEventById(id);
+
+  if (!event) notFound();
+
+  return (
+    <div className="space-y-8 max-w-lg mx-auto">
+      <h1 className="h2-bold">
+        Update event <span className="text-sm">({id})</span>
+      </h1>
+      <UpdateEventForm event={event} />
+    </div>
+  );
+};
+
+export default AdminEventUpdatePage;
--- app/admin/events/[id]/page.tsx
+++ app/admin/events/[id]/page.tsx
@@ -1,33 +1,12 @@
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
-import UpdateEventForm from "@/components/admin/update-event-form";
-import { requireAdmin } from "@/lib/auth-guard";
-import { getEventById } from "@/lib/actions/event.actions";
+import Link from "next/link";
 
-export const metadata: Metadata = {
-  title: "Update Event",
-};
-
-interface PropTypes {
-  params: Promise<{ id: string }>;
-}
-
-const AdminEventUpdatePage = async ({ params }: PropTypes) => {
-  await requireAdmin();
-  const { id } = await params;
-
-  const event = await getEventById(id);
-
-  if (!event) notFound();
-
+const EventInfo = () => {
   return (
-    <div className="space-y-8 max-w-lg mx-auto">
-      <h1 className="h2-bold">
-        Update event <span className="text-sm">({id})</span>
-      </h1>
-      <UpdateEventForm event={event} />
+    <div>
+      <div>EventInfo</div>
+      {/* <Link href=></Link> */}
     </div>
   );
 };
 
-export default AdminEventUpdatePage;
+export default EventInfo;
