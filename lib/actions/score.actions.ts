f1c94a2b54838ab452067b249b732edda5d34543
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-04-03 19:20:53 -0400

    feat: create score action

--- lib/actions/score.actions.ts
+++ lib/actions/score.actions.ts
@@ -2,8 +2,43 @@
 import { prisma } from "@/db/prisma";
 import { requireAdminAction } from "@/lib/auth-guard";
 import { formatError } from "@/lib/utils";
+import { createScoreSchema } from "@/lib/validators";
 import { UpdateScore } from "@/types";
 import { revalidatePath } from "next/cache";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+
+export async function createScore(prevState: unknown, formData: FormData) {
+  try {
+    const admin = await requireAdminAction();
+    if (!admin) throw new Error("You are not authorized!");
+    const score = createScoreSchema.parse({
+      eventId: formData.get("eventId"),
+      golferId: formData.get("golferId"),
+      score: formData.get("score"),
+      birdies: formData.get("birdies"),
+      snowmen: formData.get("snowmen"),
+      closestToPin: formData.get("closestToPin"),
+    });
+
+    await prisma.score.create({
+      data: {
+        eventId: score.eventId,
+        golferId: score.golferId,
+        score: score.score,
+        birdies: score.birdies,
+        snowmen: score.snowmen,
+        closestToPin: score.closestToPin,
+      },
+    });
+
+    revalidatePath(`/admin/events/${score.eventId}`);
+  } catch (err) {
+    if (isRedirectError(err)) {
+      throw err;
+    }
+    return { success: false, message: formatError(err) };
+  }
+}
 
 export async function getScoreById(scoreId: string | undefined) {
   if (!scoreId) throw new Error("No id passed");
