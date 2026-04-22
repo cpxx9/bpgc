bf09fade9395fecd3dd68988a7b9314ed2a3bdcb
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-04-21 21:39:43 -0400

    fix: use preview URL for preview branch

--- db/prisma.ts
+++ db/prisma.ts
@@ -13,9 +13,17 @@ if (process.env.LOCATION !== "local") {
 
   neonConfig.webSocketConstructor = ws;
 
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
+  let pool: any;
+
+  if (process.env.NODE_ENV === "production") {
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+  } else {
+    pool = new Pool({
+      connectionString: process.env.DEVDB_DATABASE_URL,
+    });
+  }
 
   const adapter = new PrismaNeon(pool);
   prisma = new PrismaClient({ adapter });
