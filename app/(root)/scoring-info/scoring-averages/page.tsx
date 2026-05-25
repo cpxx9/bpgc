77ace5e531d559e58232d85b623d86d771a494b0
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-05-25 12:33:38 -0400

    style: base style for weekly averages section

--- app/(root)/scoring-info/scoring-averages/page.tsx
+++ app/(root)/scoring-info/scoring-averages/page.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 const ScoringInfoAveragesPage = () => {
   return (
     <article className="bg-orange-50 h-full p-3">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center gap-4">
         <section className="bg-blue-300/70 p-5 min-w-[800px]">
           <h2 className="mb-5 text-2xl text-white font-bold">
             2026 Individual Scoring Averages
@@ -14,7 +14,7 @@ const ScoringInfoAveragesPage = () => {
             <IndividualScoreAverages />
           </Suspense>
         </section>
-        <section>
+        <section className="bg-slate-300/70 p-5 min-w-[800px]">
           <h2>Weekly League Scoring Averages</h2>
           <Suspense fallback={<></>}>
             <EventScoreAverages />
