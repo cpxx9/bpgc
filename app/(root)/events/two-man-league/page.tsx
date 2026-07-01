a2322fa980ca2d647dcbee0da1a272309def7ef7
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-06-30 22:30:36 -0400

    feat: begin layout for events two man team page and component for matchups

--- app/(root)/events/two-man-league/page.tsx
+++ app/(root)/events/two-man-league/page.tsx
@@ -1,9 +1,33 @@
+import splash from "@/assets/homepagesplash.avif";
+import WeeklyScoresEventAveragesSkeleton from "@/components/skeletons/weekly-scores-event-averages-skeleton";
+import WeeklyScoresIndivAveragesSkeleton from "@/components/skeletons/weekly-scores-indiv-averages-skeleton";
+import Image from "next/image";
+import { Suspense } from "react";
+
 const EventsTwoManLeaguePage = () => {
   return (
-    <>
-      <h2>Events</h2>
-      <div>Two Man League</div>
-    </>
+    <article className="p-3 relative">
+      <Image
+        src={splash}
+        alt="golf course"
+        fill
+        sizes="100vw"
+        style={{ objectFit: "cover", zIndex: -20 }}
+      />
+      <div className="-z-10 absolute inset-0 bg-black/30" />
+      <div className="flex flex-col items-center gap-4">
+        <section className="bg-slate-600/70 p-5 min-w-[800px]">
+          <h2 className="mb-5 text-2xl text-white font-bold">
+            2026 Two Man Leage Standings
+          </h2>
+          <Suspense fallback={<WeeklyScoresIndivAveragesSkeleton />}></Suspense>
+        </section>
+        <section className="bg-blue-400/70 p-5 min-w-[800px]">
+          <h2 className="mb-5 text-2xl text-white font-bold">Matchups</h2>
+          <Suspense fallback={<WeeklyScoresEventAveragesSkeleton />}></Suspense>
+        </section>
+      </div>
+    </article>
   );
 };
 
