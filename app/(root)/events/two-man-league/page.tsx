e0f05a27ed0226080e8167dd3ed6107bb60aae2e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-14 18:38:08 -0400

    feat: begin component for two man league standings, update width of sections

--- app/(root)/events/two-man-league/page.tsx
+++ app/(root)/events/two-man-league/page.tsx
@@ -1,5 +1,6 @@
 import splash from "@/assets/homepagesplash.avif";
 import Matchups from "@/components/events/matchups";
+import TwoManStandings from "@/components/events/two-man-standings";
 import MatchupsSkeleton from "@/components/skeletons/matchups-skeleton";
 import WeeklyScoresIndivAveragesSkeleton from "@/components/skeletons/weekly-scores-indiv-averages-skeleton";
 import Image from "next/image";
@@ -16,20 +17,24 @@ const EventsTwoManLeaguePage = () => {
         style={{ objectFit: "cover", zIndex: -20 }}
       />
       <div className="-z-10 absolute inset-0 bg-black/30" />
-      <div className="flex flex-col items-center gap-4">
-        <section className="bg-slate-600/70 p-5 min-w-[800px]">
-          <h2 className="mb-5 text-2xl text-white font-bold">
-            2026 Two Man Leage Standings
-          </h2>
-          <Suspense fallback={<WeeklyScoresIndivAveragesSkeleton />}></Suspense>
-        </section>
-        <section className="bg-blue-400/70 p-5 min-w-[800px] grid grid-cols-[1fr_auto] gap-x-8">
-          <h2 className="text-2xl text-white font-bold">Matchups</h2>
-          <h2 className="text-2xl text-white font-bold">Teams</h2>
-          <Suspense fallback={<MatchupsSkeleton />}>
-            <Matchups />
-          </Suspense>
-        </section>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col gap-4 w-fit">
+          <section className="w-full bg-slate-600/70 p-5 max-w-[1000px] flex flex-col gap-5">
+            <h2 className="text-2xl text-white font-bold">
+              2026 Two Man Leage Standings
+            </h2>
+            <Suspense fallback={<WeeklyScoresIndivAveragesSkeleton />}>
+              <TwoManStandings />
+            </Suspense>
+          </section>
+          <section className="w-full bg-blue-400/70 p-5 max-w-[1000px] grid grid-cols-[1fr_auto] gap-x-8">
+            <h2 className="text-2xl text-white font-bold">Matchups</h2>
+            <h2 className="text-2xl text-white font-bold">Teams</h2>
+            <Suspense fallback={<MatchupsSkeleton />}>
+              <Matchups />
+            </Suspense>
+          </section>
+        </div>
       </div>
     </article>
   );
---
+++ components/events/two-man-standings.tsx
@@ -0,0 +1,19 @@
+const TwoManStandings = () => {
+  return (
+    <>
+      <article>TwoManStandings</article>
+      <article>
+        <p>
+          * Standings will reflect the two lowest scores dropped after week 6
+        </p>
+        <p>
+          ** Top 4 teams automatically make the Round of 8 playoff matches;
+          teams ranked #5 - #12 play on wildcard weekend for the final 4 spots
+          **
+        </p>
+      </article>
+    </>
+  );
+};
+
+export default TwoManStandings;
