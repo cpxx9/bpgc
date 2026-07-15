ff3512539464ad714a6677dfac8fe5ef5583940d
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-14 20:02:42 -0400

    refactor: move details to non parent page component

--- app/(root)/events/two-man-league/page.tsx
+++ app/(root)/events/two-man-league/page.tsx
@@ -26,6 +26,28 @@ const EventsTwoManLeaguePage = () => {
             <Suspense fallback={<WeeklyScoresIndivAveragesSkeleton />}>
               <TwoManStandings />
             </Suspense>
+            <section className="font-semibold">
+              <p>
+                *
+                <em className="underline ml-4">
+                  Standings will reflect the two lowest scores dropped after
+                  week 6
+                </em>
+              </p>
+              <p>
+                **
+                <span className="mx-2">
+                  Top <span className="text-[rgb(121,175,19)]">4 teams</span>{" "}
+                  automatically make the Round of 8 playoff matches; teams
+                  ranked{" "}
+                  <span className="text-[rgb(114,198,229)]">
+                    #5 - #12 play on wildcard
+                  </span>{" "}
+                  weekend for the final 4 spots
+                </span>
+                **
+              </p>
+            </section>
           </section>
           <section className="w-full bg-blue-400/70 p-5 max-w-[1000px] grid grid-cols-[1fr_auto] gap-x-8">
             <h2 className="text-2xl text-white font-bold">Matchups</h2>
--- components/events/two-man-standings.tsx
+++ components/events/two-man-standings.tsx
@@ -1,28 +1,18 @@
-const TwoManStandings = () => {
+import { getTwoManTeamsStandingsPublic } from "@/lib/actions/two-man-team.actions";
+
+const WEEKS = 10;
+
+const TwoManStandings = async () => {
+  const result = await getTwoManTeamsStandingsPublic();
+  if (!result.success) {
+    return;
+    <p>No data found</p>;
+  }
+
   return (
-    <>
-      <section>TwoManStandings</section>
-      <section className="font-semibold">
-        <p>
-          *
-          <em className="underline ml-4">
-            Standings will reflect the two lowest scores dropped after week 6
-          </em>
-        </p>
-        <p>
-          **
-          <span className="mx-2">
-            Top <span className="text-[rgb(121,175,19)]">4 teams</span>{" "}
-            automatically make the Round of 8 playoff matches; teams ranked{" "}
-            <span className="text-[rgb(114,198,229)]">
-              #5 - #12 play on wildcard
-            </span>{" "}
-            weekend for the final 4 spots
-          </span>
-          **
-        </p>
-      </section>
-    </>
+    <section>
+      <p>test</p>
+    </section>
   );
 };
 
