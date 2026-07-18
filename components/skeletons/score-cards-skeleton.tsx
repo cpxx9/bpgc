268acc369dfcdd3440e029f3cb33e886730bc0a7
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-18 13:35:14 -0400

    feat: skeleton for score cards

---
+++ components/skeletons/score-cards-skeleton.tsx
@@ -0,0 +1,43 @@
+const ScoreCardsSkeleton = () => {
+  return (
+    <section className="mx-auto max-w-5xl">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {/* Card */}
+        <article className="flex flex-col">
+          <div className="bg-sky-600/70 h-44 flex flex-col justify-between p-4 text-center text-white">
+            <h2 className="font-bold text-2xl">Birdie Busters</h2>
+            <p className="font-semibold">Clubhouse Leader</p>
+          </div>
+
+          <div className="bg-neutral-700 p-4 text-white">
+            {/* standings list */}
+          </div>
+        </article>
+
+        <article className="flex flex-col">
+          <div className="bg-sky-600/70 h-44 flex flex-col justify-between p-4 text-center text-white">
+            <h2 className="font-bold text-2xl">Closest-to-Pin / Hole-in-One</h2>
+            <p className="font-semibold">Clubhouse Leader</p>
+          </div>
+
+          <div className="bg-neutral-700 p-4 text-white">
+            {/* standings list */}
+          </div>
+        </article>
+
+        <article className="flex flex-col">
+          <div className="bg-sky-600/70 h-44 flex flex-col justify-between p-4 text-center text-white">
+            <h2 className="font-bold text-2xl">Snowmen</h2>
+            <p className="font-semibold">Clubhouse Leader</p>
+          </div>
+
+          <div className="bg-neutral-700 p-4 text-white">
+            {/* standings list */}
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default ScoreCardsSkeleton;
