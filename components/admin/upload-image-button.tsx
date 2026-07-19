6ee43ecbe08081c9e3b4b13c40ba14d31f5fa6b6
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-19 18:59:36 -0400

    style: better style for upload button

--- components/admin/upload-image-button.tsx
+++ components/admin/upload-image-button.tsx
@@ -7,22 +7,33 @@ const UploadImageButton = () => {
   const { toast } = useToast();
 
   return (
-    <UploadButton
-      className="absolute top-2 right-2"
-      endpoint="imageUploader"
-      onClientUploadComplete={(res: { url: string }[]) => {
-        toast({
-          variant: "default",
-          description: `Image uploaded to ${res[0].url}`,
-        });
-      }}
-      onUploadError={(error: Error) => {
-        toast({
-          variant: "destructive",
-          description: `ERROR! ${error.message}`,
-        });
-      }}
-    />
+    <div className="absolute top-2 right-2 flex items-center gap-1">
+      <p>Upload images to the gallery</p>
+      <UploadButton
+        endpoint="imageUploader"
+        appearance={{
+          button: {
+            border: "1px solid gray",
+            color: "gray",
+            padding: "1em",
+          },
+          container: {},
+          allowedContent: {},
+        }}
+        onClientUploadComplete={(res: { url: string }[]) => {
+          toast({
+            variant: "default",
+            description: `Image uploaded to ${res[0].url}`,
+          });
+        }}
+        onUploadError={(error: Error) => {
+          toast({
+            variant: "destructive",
+            description: `ERROR! ${error.message}`,
+          });
+        }}
+      />
+    </div>
   );
 };
 
