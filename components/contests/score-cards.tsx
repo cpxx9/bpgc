5eddc79e9c792ff2185dd4b0ca83d1b4b4aef1c8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-19 18:13:48 -0400

    fix: better check for leaders, display no leader yet if there are no scores

--- components/contests/score-cards.tsx
+++ components/contests/score-cards.tsx
@@ -22,25 +22,35 @@ const ScoreCards = async () => {
           <div className="bg-sky-600 flex flex-col gap-4 justify-between p-4 text-center text-white">
             <h2 className="font-bold text-3xl">Birdie Busters</h2>
             <div className="text-gray-600 hidden md:block">
-              <h3 className="text-2xl font-semibold">{`${birdiesLeader.firstName} ${birdiesLeader.lastName}`}</h3>
-              <h3 className="text-xl font-semibold">
-                {birdiesLeader.average.toFixed(2)}
-              </h3>
+              {birdiesLeader ? (
+                <>
+                  <h3 className="text-2xl font-semibold">{`${birdiesLeader.firstName} ${birdiesLeader.lastName}`}</h3>
+                  <h3 className="text-xl font-semibold">
+                    {birdiesLeader.average.toFixed(2)}
+                  </h3>
+                </>
+              ) : (
+                <h3 className="text-xl font-semibold">No winner yet</h3>
+              )}
             </div>
             <p className="font-semibold hidden md:block">Clubhouse Leader</p>
           </div>
 
           <div className="bg-neutral-700 p-4 text-white">
             <ul>
-              {contests.data.birdies.map((golfer) => (
-                <li
-                  key={golfer.id}
-                  className="flex justify-between font-semibold text-lg"
-                >
-                  <p>{`${golfer.firstName} ${golfer.lastName}`}</p>
-                  <p>{golfer.average.toFixed(2)}</p>
-                </li>
-              ))}
+              {contests.data.birdies.length > 0 ? (
+                contests.data.birdies.map((golfer) => (
+                  <li
+                    key={golfer.id}
+                    className="flex justify-between font-semibold text-lg"
+                  >
+                    <p>{`${golfer.firstName} ${golfer.lastName}`}</p>
+                    <p>{golfer.average.toFixed(2)}</p>
+                  </li>
+                ))
+              ) : (
+                <li className="font-semibold text-lg">No scores yet</li>
+              )}
             </ul>
           </div>
         </article>
@@ -67,19 +77,23 @@ const ScoreCards = async () => {
 
           <div className="bg-neutral-700 p-4 text-white">
             <ul>
-              {contests.data.closestToPin.map((golfer) => {
-                const feetAndInches = convertFloatToFeet(golfer.lowest);
+              {contests.data.closestToPin.length > 0 ? (
+                contests.data.closestToPin.map((golfer) => {
+                  const feetAndInches = convertFloatToFeet(golfer.lowest);
 
-                return (
-                  <li
-                    key={golfer.id}
-                    className="flex justify-between font-semibold text-lg"
-                  >
-                    <p>{`${golfer.firstName} ${golfer.lastName}`}</p>
-                    <p>{`${feetAndInches.feet}' ${feetAndInches.inches}"`}</p>
-                  </li>
-                );
-              })}
+                  return (
+                    <li
+                      key={golfer.id}
+                      className="flex justify-between font-semibold text-lg"
+                    >
+                      <p>{`${golfer.firstName} ${golfer.lastName}`}</p>
+                      <p>{`${feetAndInches.feet}' ${feetAndInches.inches}"`}</p>
+                    </li>
+                  );
+                })
+              ) : (
+                <li className="font-semibold text-lg">No scores yet</li>
+              )}
             </ul>
           </div>
         </article>
@@ -88,25 +102,35 @@ const ScoreCards = async () => {
           <div className="bg-sky-600 flex flex-col gap-4 justify-between p-4 text-center text-white">
             <h2 className="font-bold text-3xl">Snowmen</h2>
             <div className="text-gray-600 hidden md:block">
-              <h3 className="text-2xl font-semibold">{`${snowmenLeader.firstName} ${snowmenLeader.lastName}`}</h3>
-              <h3 className="text-xl font-semibold">
-                {snowmenLeader.average.toFixed(2)}
-              </h3>
+              {snowmenLeader ? (
+                <>
+                  <h3 className="text-2xl font-semibold">{`${snowmenLeader.firstName} ${snowmenLeader.lastName}`}</h3>
+                  <h3 className="text-xl font-semibold">
+                    {snowmenLeader.average.toFixed(2)}
+                  </h3>
+                </>
+              ) : (
+                <h3 className="text-xl font-semibold">No winner yet</h3>
+              )}
             </div>
             <p className="font-semibold hidden md:block">Clubhouse Leader</p>
           </div>
 
           <div className="bg-neutral-700 p-4 text-white">
             <ul>
-              {contests.data.snowmen.map((golfer) => (
-                <li
-                  key={golfer.id}
-                  className="flex justify-between font-semibold text-lg"
-                >
-                  <p>{`${golfer.firstName} ${golfer.lastName}`}</p>
-                  <p>{golfer.average.toFixed(2)}</p>
-                </li>
-              ))}
+              {contests.data.snowmen.length > 0 ? (
+                contests.data.snowmen.map((golfer) => (
+                  <li
+                    key={golfer.id}
+                    className="flex justify-between font-semibold text-lg"
+                  >
+                    <p>{`${golfer.firstName} ${golfer.lastName}`}</p>
+                    <p>{golfer.average.toFixed(2)}</p>
+                  </li>
+                ))
+              ) : (
+                <li className="font-semibold text-lg">No scores yet</li>
+              )}
             </ul>
           </div>
         </article>
