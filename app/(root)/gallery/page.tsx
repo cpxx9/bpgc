883e3bc8ec95bab89ff292a8413e509f93b2b149
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-21 07:28:41 -0400

    feat: carousel mostly made. needs some style changes

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -1,7 +1,11 @@
+import ImageGallery from "@/components/gallery/image-gallery";
+
 const GalleryPage = () => {
   return (
     <article className="p-3 min-h-[100%] flex flex-col justify-center items-center">
-      <div>test</div>
+      <div className="">
+        <ImageGallery />
+      </div>
     </article>
   );
 };
--- components/gallery/image-carousel.tsx
+++ components/gallery/image-carousel.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -10,11 +12,12 @@ import {
 import { DbImage } from "@/types";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
+import Link from "next/link";
 
 const ImageCarousel = ({ data }: { data: DbImage[] }) => {
   return (
     <Carousel
-      className="w-full mb-12"
+      className="w-[90vw] max-w-[calc((100dvh-14rem)*16/9)] mx-auto"
       opts={{
         loop: true,
       }}
@@ -29,21 +32,23 @@ const ImageCarousel = ({ data }: { data: DbImage[] }) => {
       <CarouselContent>
         {data.map((DbImage: DbImage) => (
           <CarouselItem key={DbImage.id}>
-            <div className="relative mx-auto">
-              <Image
-                src={DbImage.url!}
-                alt={`Image from cdn: ${DbImage.key}`}
-                height="0"
-                width="0"
-                sizes="100vw"
-                className="w-full h-auto"
-              />
-              <div className="absolute inset-0 flex items-end justify-center">
-                <h2 className="bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white">
-                  Let's see what this is
-                </h2>
+            <Link href={DbImage.url}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex p-0 overflow-hidden round-md">
+                    <AspectRatio ratio={16 / 9} className="w-full bg-muted">
+                      <Image
+                        src={DbImage.url!}
+                        alt={`Image from cdn: ${DbImage.key}`}
+                        fill
+                        sizes="(max-w-xl) 100%"
+                        className="object-cover"
+                      />
+                    </AspectRatio>
+                  </CardContent>
+                </Card>
               </div>
-            </div>
+            </Link>
           </CarouselItem>
         ))}
       </CarouselContent>
