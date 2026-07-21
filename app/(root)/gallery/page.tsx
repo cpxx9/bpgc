95aa1a0bd03d39a37caec6b40c41b4cba08a69a8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-21 06:48:30 -0400

    feat: base layout for gallery

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -1,5 +1,9 @@
 const GalleryPage = () => {
-  return <div>Gallery</div>;
+  return (
+    <article className="p-3 min-h-[100%] flex flex-col justify-center items-center">
+      <div>test</div>
+    </article>
+  );
 };
 
 export default GalleryPage;
