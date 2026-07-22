d2de3f457b7fcaf89085c1a11ca2029c880fb56e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-22 06:43:44 -0400

    feat: add thumbnails

--- app/(root)/gallery/page.tsx
+++ app/(root)/gallery/page.tsx
@@ -3,9 +3,7 @@ import ImageGallery from "@/components/gallery/image-gallery";
 const GalleryPage = () => {
   return (
     <article className="h-full overflow-hidden flex flex-col justify-center items-center p-3">
-      <div className="">
-        <ImageGallery />
-      </div>
+      <ImageGallery />
     </article>
   );
 };
--- components/gallery/image-carousel.tsx
+++ components/gallery/image-carousel.tsx
@@ -4,6 +4,7 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
+  CarouselApi,
   CarouselContent,
   CarouselItem,
   CarouselNext,
@@ -13,48 +14,91 @@ import { DbImage } from "@/types";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const ImageCarousel = ({ data }: { data: DbImage[] }) => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  const handleApiChange = (newApi: CarouselApi) => {
+    setApi(newApi);
+
+    if (newApi) {
+      setCurrent(newApi.selectedScrollSnap());
+
+      newApi.on("select", () => {
+        setCurrent(newApi.selectedScrollSnap());
+      });
+    }
+  };
+
   return (
-    <Carousel
-      className="w-[95vw] max-w-[calc((100vh-20rem)*16/9)] mx-auto"
-      opts={{
-        loop: true,
-      }}
-      plugins={[
-        Autoplay({
-          delay: 10000,
-          stopOnInteraction: true,
-          stopOnMouseEnter: true,
-        }),
-      ]}
-    >
-      <CarouselContent>
-        {data.map((DbImage: DbImage) => (
-          <CarouselItem key={DbImage.id}>
-            <Link href={DbImage.url} target="_blank">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex p-0 overflow-hidden round-md">
-                    <AspectRatio ratio={16 / 9} className="w-full bg-muted">
-                      <Image
-                        src={DbImage.url!}
-                        alt={`Image from cdn: ${DbImage.key}`}
-                        fill
-                        sizes="(max-w-xl) 100%"
-                        className="object-cover"
-                      />
-                    </AspectRatio>
-                  </CardContent>
-                </Card>
-              </div>
-            </Link>
-          </CarouselItem>
+    <div>
+      <Carousel
+        className="w-[95vw] max-w-[calc((100vh-20rem)*16/9)] mx-auto"
+        opts={{
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 3000,
+            stopOnInteraction: true,
+            stopOnMouseEnter: true,
+          }),
+        ]}
+        setApi={handleApiChange}
+      >
+        <CarouselContent>
+          {data.map((DbImage: DbImage) => (
+            <CarouselItem key={DbImage.id}>
+              <Link href={DbImage.url} target="_blank">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex p-0 overflow-hidden round-md">
+                      <AspectRatio ratio={16 / 9} className="w-full bg-muted">
+                        <Image
+                          src={DbImage.url!}
+                          alt={`Image from cdn: ${DbImage.key}`}
+                          fill
+                          sizes="(max-w-xl) 100%"
+                          className="object-cover"
+                        />
+                      </AspectRatio>
+                    </CardContent>
+                  </Card>
+                </div>
+              </Link>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="flex gap-2">
+        {/* <Carousel opts={{ align: "start" }}>
+          <CarouselContent></CarouselContent>
+        </Carousel> */}
+        {data.map((DbImage: DbImage, index) => (
+          <Card
+            key={DbImage.id}
+            className={`w-[67px] h-[50px] ${current === index ? "border-2 border-blue-500" : ""}`}
+            onClick={() => api?.scrollTo(index)}
+          >
+            <CardContent className="flex p-0 overflow-hidden round-md">
+              <AspectRatio ratio={4 / 3} className="w-full bg-muted">
+                <Image
+                  src={DbImage.url!}
+                  alt={`Image from cdn: ${DbImage.key}`}
+                  sizes="(max-w-xl) 100%"
+                  fill
+                  className="object-cover"
+                />
+              </AspectRatio>
+            </CardContent>
+          </Card>
         ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+      </div>
+    </div>
   );
 };
 
