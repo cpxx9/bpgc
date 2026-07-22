d0096fbe5f417a66c6789d70e1a4663c9a5b4013
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-22 08:10:17 -0400

    feat: if image array is 0, display no images found message

--- components/gallery/image-gallery.tsx
+++ components/gallery/image-gallery.tsx
@@ -3,7 +3,7 @@ import { getDisplayedImagesPublic } from "@/lib/actions/image.actions";
 
 const ImageGallery = async () => {
   const result = await getDisplayedImagesPublic();
-  if (!result.success || !result.data) {
+  if (!result.success || result.data.length < 1) {
     return <>No images found!</>;
   }
 
