df944569e240b36fdd4430e5dea75964390cc43d
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-28 22:12:32 -0400

    feat: fix sign in url callback

--- components/shared/sign-in-link.tsx
+++ components/shared/sign-in-link.tsx
@@ -4,16 +4,20 @@ import { Button } from "@/components/ui/button";
 import { UserIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const SignInLink = () => {
   const { data: session, status } = useSession();
+  const pathname = usePathname();
 
   if (status === "loading" || session) return null;
+
+  const href = `/sign-in?callbackUrl=${encodeURIComponent(pathname)}`;
 
   if (!session) {
     return (
       <Button asChild>
-        <Link href="/sign-in">
+        <Link href={href}>
           <UserIcon /> Sign in
         </Link>
       </Button>
