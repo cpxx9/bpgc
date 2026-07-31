dff42ecb903657d3967f9b4ed22431ceeee9dfc8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-31 06:25:07 -0400

    feat: add basic site map change freq

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,27 +5,34 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
       url: `${SERVER_URL}/`,
+      changeFrequency: "weekly",
     },
     {
       url: `${SERVER_URL}/about`,
     },
     {
       url: `${SERVER_URL}/schedule`,
+      changeFrequency: "yearly",
     },
     {
       url: `${SERVER_URL}/scoring-info/weekly-scores`,
+      changeFrequency: "weekly",
     },
     {
       url: `${SERVER_URL}/scoring-info/scoring-averages`,
+      changeFrequency: "weekly",
     },
     {
       url: `${SERVER_URL}/events/two-man-league`,
+      changeFrequency: "weekly",
     },
     {
       url: `${SERVER_URL}/events/club-championship`,
+      changeFrequency: "yearly",
     },
     {
       url: `${SERVER_URL}/contests`,
+      changeFrequency: "weekly",
     },
     {
       url: `${SERVER_URL}/gallery`,
