62328a61dce0c37967a0d66cb4e53d43142b068b
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-07-31 06:22:55 -0400

    feat: add basic site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,31 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
+      url: `${SERVER_URL}/`,
+    },
+    {
       url: `${SERVER_URL}/about`,
+    },
+    {
+      url: `${SERVER_URL}/schedule`,
+    },
+    {
+      url: `${SERVER_URL}/scoring-info/weekly-scores`,
+    },
+    {
+      url: `${SERVER_URL}/scoring-info/scoring-averages`,
+    },
+    {
+      url: `${SERVER_URL}/events/two-man-league`,
+    },
+    {
+      url: `${SERVER_URL}/events/club-championship`,
+    },
+    {
+      url: `${SERVER_URL}/contests`,
+    },
+    {
+      url: `${SERVER_URL}/gallery`,
     },
   ];
 }
