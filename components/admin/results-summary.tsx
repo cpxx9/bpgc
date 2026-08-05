8c11376d2688bf2fb5f32f1917ec187b7f2ea617
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2026-08-04 21:58:34 -0400

    feat: better display, show which results are displaying on pagination

--- components/admin/results-summary.tsx
+++ components/admin/results-summary.tsx
@@ -11,19 +11,26 @@ const ResultsSummary = ({
   totalPages: number;
   page: number;
   query?: string;
-}) => (
-  <>
-    <div className="flex items-center justify-between gap-2 mb-2">
-      <p className="text-muted-foreground">
-        {totalCount < PAGE_SIZE && !query
-          ? "Displaying all results..."
-          : totalCount > 0
-            ? `${totalCount} result${totalCount === 1 ? "" : "s"}${query ? ` for "${query}"` : ""}`
-            : "No search results found..."}
-      </p>
-    </div>
-    {totalPages > 1 && <Pagination page={page} totalPages={totalPages} />}
-  </>
-);
+}) => {
+  const start = (page - 1) * PAGE_SIZE + 1;
+  const end = Math.min(page * PAGE_SIZE, totalCount);
+
+  return (
+    <>
+      <div className="flex items-center justify-between gap-2 mb-2">
+        <p className="text-muted-foreground">
+          {query
+            ? totalCount > 0
+              ? `${totalCount} result${totalCount === 1 ? "" : "s"} for "${query}"`
+              : "No search results found..."
+            : totalCount <= PAGE_SIZE
+              ? "Displaying all results..."
+              : `Displaying ${start}-${end} of ${totalCount} results`}
+        </p>
+      </div>
+      {totalPages > 1 && <Pagination page={page} totalPages={totalPages} />}
+    </>
+  );
+};
 
 export default ResultsSummary;
